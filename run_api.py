c3078fc9d2fb4c0e43bd4f9da3b993642f082041
fluoryynx <88953782+fluoryynx@users.noreply.github.com>
2026-01-07 21:02:47 +0700

    Create run_api.py

---
+++ run_api.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+Script to run the API server
+"""
+from api import create_app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True, host='0.0.0.0', port=5001)
